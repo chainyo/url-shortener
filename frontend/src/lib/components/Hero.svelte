958f6e43c11736ea6df7d3ece6ec05ef3fd52357
chainyo <t.chaigneau.tc@gmail.com>
2023-07-02 18:26:35 +0200

    create front base

---
+++ frontend/src/lib/components/Hero.svelte
@@ -0,0 +1,50 @@
+<div class="hero min-h-screen bg-base-200 flex flex-col place-content-center">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">URL ⚡ SHRTNR</h1>
+            <p class="py-6">Shorten and share long URLs in a snap!</p>
+            <div class="join">
+                <input class="input input-bordered join-item input-accent w-full max-w-xs rounded-full" placeholder="Your looooong url..."/>
+                <button class="btn btn-outline btn-accent join-item rounded-r-full">TL;DR</button>
+              </div>
+        </div>
+    </div>
+    <div class="stats shadow mt-16 md:grid flex flex-col">
+
+        <div class="stat">
+            <div class="stat-figure text-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                </svg>
+            </div>
+            <div class="stat-title">GitHub stars</div>
+            <div class="stat-value text-primary">12.3K</div>
+            <div class="stat-desc">so close to be #1</div>
+        </div>
+        
+        <div class="stat">
+            <div class="stat-figure text-secondary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+                </svg>
+            </div>
+            <div class="stat-title">URL Shortened</div>
+            <div class="stat-value text-secondary">2.6M</div>
+            <div class="stat-desc">and still free</div>
+        </div>
+        
+        <div class="stat">
+            <div class="stat-figure text-secondary">
+                <div class="avatar">
+                <div class="w-16 rounded-full ring ring-primary ring-offset-base-100">
+                    <img src="/chainyo.jpg" alt="creator portrait" />
+                </div>
+                </div>
+            </div>
+            <div class="stat-value">👋</div>
+            <div class="stat-title">@chainyo</div>
+            <div class="stat-desc text-secondary">ML Engineer</div>
+        </div>
+        
+      </div>
+</div>
